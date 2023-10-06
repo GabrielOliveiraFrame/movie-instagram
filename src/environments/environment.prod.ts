dbed576e88f520492fde4ca3ea266be9e3e6402f
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-06 14:07:02 -0300

    Update prod env for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_POSTS_URL: 'http://localhost:3000/posts',
   API_COMMENTS_URL: 'http://localhost:3000/comments',
-  API_LIKES_URL: 'http://localhost:3000/likes
+  API_LIKES_URL: 'http://localhost:3000/likes'
 };
