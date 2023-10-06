d836f44752fd88b2ba7a5e5b8669a77adea2f858
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-06 17:27:11 -0300

    Update vercel json-server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_POSTS_URL: 'https://json-server-tech-vercel-oa6szo3yf-gabriels-projects-943d9c5c.vercel.app/posts',
-  API_COMMENTS_URL: 'https://json-server-tech-vercel-oa6szo3yf-gabriels-projects-943d9c5c.vercel.app/comments',
-  API_LIKES_URL: 'https://json-server-tech-vercel-oa6szo3yf-gabriels-projects-943d9c5c.vercel.app/likes'
+  API_POSTS_URL: 'https://json-server-tech-vercel-2xlj8d4po-gabriels-projects-943d9c5c.vercel.app/posts',
+  API_COMMENTS_URL: 'https://json-server-tech-vercel-2xlj8d4po-gabriels-projects-943d9c5c.vercel.app/comments',
+  API_LIKES_URL: 'https://json-server-tech-vercel-2xlj8d4po-gabriels-projects-943d9c5c.vercel.app/likes'
 };
