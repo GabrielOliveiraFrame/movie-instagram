6ca10d8e04bdb9e141bce6c22cd3a8784404c6b1
techGabrielBr <brgabrieltw@gmail.com>
2023-10-06 15:31:49 -0300

    Update prod env for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_POSTS_URL: 'http://localhost:3000/posts',
-  API_COMMENTS_URL: 'http://localhost:3000/comments',
-  API_LIKES_URL: 'http://localhost:3000/likes'
+  API_POSTS_URL: 'https://json-server-tech-vercel-pky34bqff-gabriels-projects-943d9c5c.vercel.app/posts',
+  API_COMMENTS_URL: 'https://json-server-tech-vercel-pky34bqff-gabriels-projects-943d9c5c.vercel.app/comments',
+  API_LIKES_URL: 'https://json-server-tech-vercel-pky34bqff-gabriels-projects-943d9c5c.vercel.app/likes'
 };
