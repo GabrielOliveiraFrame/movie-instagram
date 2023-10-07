d97e3d7c9e8bd7e046a969eeba40e2461a52ae82
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-07 08:06:41 -0300

    Add Render url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_POSTS_URL: "https://json-server-tech-vercel.vercel.app/posts",
-  API_COMMENTS_URL: "https://json-server-tech-vercel.vercel.app/comments",
-  API_LIKES_URL: "https://json-server-tech-vercel.vercel.app/likes"
+  API_POSTS_URL: "https://json-server-tech-vercel.onrender.com/posts",
+  API_COMMENTS_URL: "https://json-server-tech-vercel.onrender.com/comments",
+  API_LIKES_URL: "https://json-server-tech-vercel.onrender.com/likes"
 };
